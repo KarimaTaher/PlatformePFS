44a0aeb6207d01b1183b47d334db2fe8e72711b0
noufissazidane <zidanenoufissa@gmail.com>
2025-05-18 16:36:27 +0100

    Modifications locales : app.py, styles.css, ajout de prediction.html

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+import os
+from datetime import datetime
+import random
+
 
 app= Flask(__name__)
+
+
+@app.route('/predictions')
+def predictions():
+   return render_template('prediction.html')
+
 
 @app.route('/')
 def homepage():
    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug=True, host='127.0.0.1', port=5001)
+
+
