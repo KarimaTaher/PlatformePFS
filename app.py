7fe61bba634deb95f8023b3296804b1ddaa301b4
chaimae-zemri <c.zemri.26@gmail.com>
2025-05-24 15:24:41 +0200

    graphs in modals task is done

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template
-
 app= Flask(__name__)
 
 @app.route('/')
 def homepage():
    return render_template('index.html')
+
+@app.route('/graphs')
+def show_graphs():
+    return render_template('graphs.html', )
