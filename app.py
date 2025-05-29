d2a3c53a75afb1e4e2fef370b148de7febcde39d
chaimae-zemri <c.zemri.26@gmail.com>
2025-05-29 15:00:26 +0200

    graph modals commit

--- app.py
+++ app.py
@@ -4,7 +4,3 @@ app= Flask(__name__)
 @app.route('/')
 def homepage():
    return render_template('index.html')
-
-@app.route('/graphs')
-def show_graphs():
-    return render_template('graphs.html', )
